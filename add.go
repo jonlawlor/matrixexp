7470edd85d16db7bd0f549fa67b1d5a0fe94c474
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2015-07-28 23:23:36 -0400

    Add files for initial project structure
    Each matrix expression will have its own go file, for now.

---
+++ add.go
@@ -0,0 +1,5 @@
+// Copyright 2015 Jonathan J Lawlor. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package matrixexp
